4ef4e1e5571a625b59be2f045a9d26b97b5cb677
leopiccionia <leopiccionia@gmail.com>
2024-08-16 12:45:12 -0300

    Move some stuff from migration plugin to theme

--- includes/crm.php
+++ includes/crm.php
@@ -91,124 +91,11 @@ function generate_unique_user_login( string $user_name ) {
     }
 }
 
-function get_pmpro_level_id( int $post_id, string $level_name ) {
-    $level_slugs = [
-        'Conexão' => 'conexao',
-        'Essencial' => 'essencial',
-        'Institucional' => 'institucional',
-        'Vivência' => 'vivencia',
-    ];
-
-    $level_slug = $level_slugs[ $level_name ] ?? null;
-
-    if ( empty( $level_slug ) ) {
-        return null;
-    }
-
-    return \hacklabr\get_pmpro_level_options( $post_id )[ $level_slug ] ?? null;
-}
-
 function set_hacklab_as_current_user() {
     $user = get_user_by( 'login', 'hacklab' );
     if ( ! empty( $user ) ) {
         wp_set_current_user( $user->ID, $user->user_login );
     }
-}
-
-function import_account( Entity $account, bool $force_update = false ) {
-    $account_id = $account->Id;
-    $attributes = $account->Attributes;
-    $formatted = $account->FormattedValues;
-
-    $post_meta = crm\parse_account_into_post_meta( $account );
-
-    if ( crm\is_active_account( $account ) ) {
-        cli_log( "Importing account {$post_meta['nome_fantasia']} — {$account->Id}", 'debug' );
-    } else {
-        cli_log( "Skipping account {$post_meta['nome_fantasia']} — {$account->Id}", 'debug' );
-        return null;
-    }
-
-    $existing_posts = get_posts( [
-        'post_type' => 'organizacao',
-        'meta_query' => [
-            [ 'key' => '_ethos_crm_account_id', 'value' => $account_id ],
-        ],
-    ] );
-
-    if ( empty( $existing_posts ) ) {
-        $post_parent = 0;
-
-        if ( crm\is_subsidiary_company( $account ) ) {
-            $post_parent = crm\get_post_id_by_account( $attributes['parentaccountid']->Id ) ?? 0;
-        }
-
-        $post_id = wp_insert_post( [
-            'post_type' => 'organizacao',
-            'post_title' => $post_meta['nome_fantasia'],
-            'post_content' => '',
-            'post_status' => 'publish',
-            'post_parent' => $post_parent,
-            'meta_input' => $post_meta,
-        ] );
-
-        if ( ! empty( $attributes['primarycontactid'] ) && ! empty( $formatted['fut_pl_tipo_associacao'] ) ) {
-            // Update author after post creation to avoid infinite loop
-            set_primary_contact( $post_id, $attributes['primarycontactid']->Id, $formatted['fut_pl_tipo_associacao'] );
-        }
-
-        if ( ! empty( $attributes['fut_lk_contato_alternativo'] ) ) {
-            set_secondary_contact( $post_id, $attributes['fut_lk_contato_alternativo']->Id );
-        }
-
-        if ( ! empty( $attributes['fut_lk_contato_alternativo2'] ) ) {
-            set_secondary_contact( $post_id, $attributes['fut_lk_contato_alternativo2']->Id );
-        }
-
-        if ( ! empty( $attributes['i4d_aprovador_cortesia'] ) ) {
-            set_approver( $post_id, $attributes['i4d_aprovador_cortesia']->Id );
-        }
-
-        // @TODO Set featured image
-
-        if ( class_exists( '\WP_CLI' ) ) {
-            cli_log( "Created post with ID = {$post_id}", 'debug' );
-        }
-
-        return $post_id;
-    }
-
-    if ( $force_update ) {
-        wp_update_post( [
-            'ID' => $existing_posts[0]->ID,
-            'post_title' => $post_meta['nome_fantasia'],
-            'meta_input' => $post_meta,
-        ] );
-    }
-
-    return $existing_posts[0]->ID;
-}
-
-function get_contact( string $contact_id, string $account_id ) {
-    $existing_users = get_users( [
-        'meta_query' => [
-            [ 'key' => '_ethos_crm_account_id', 'value' => $account_id ],
-            [ 'key' => '_ethos_crm_contact_id', 'value' => $contact_id ],
-        ],
-    ] );
-
-    if ( empty( $existing_users ) ) {
-        $account = \hacklabr\get_crm_entity_by_id( 'account', $account_id );
-        $contact = \hacklabr\get_crm_entity_by_id( 'contact', $contact_id );
-
-        if ( ! empty( $contact ) ) {
-            return import_contact( $contact, $account, false );
-        }
-    } else {
-        return $existing_users[0]->ID;
-    }
-
-    return null;
 }
 
 function import_contact( Entity $contact, Entity|null $account = null, bool $force_update = false ) {
@@ -287,10 +174,6 @@ function import_contact( Entity $contact, Entity|null $account = null, bool $for
     return $existing_users[0]->ID;
 }
 
-function approve_contact( int $user_id, int $level_id ) {
-    return \PMPro_Approvals::approveMember( $user_id, $level_id, true );
-}
-
 function add_contact_to_organization( int $user_id, int $post_id ) {
     $existing_group_id = get_user_meta( $user_id, '_pmpro_group', true );
     if ( ! empty( $existing_group_id ) ) {
@@ -304,70 +187,10 @@ function add_contact_to_organization( int $user_id, int $post_id ) {
 
         update_user_meta( $user_id, '_pmpro_group', $group_id );
 
-        approve_contact( $user_id, $membership->group_child_level_id );
+        \hacklabr\approve_user( $user_id, $membership->group_child_level_id );
     }
 
     return $group_id ?: null;
-}
-
-function set_primary_contact( int $post_id, string $contact_id, string $level_name ) {
-    $existing_group_id = get_post_meta( $post_id, '_pmpro_group', true );
-    if ( ! empty( $existing_group_id ) ) {
-        return (int) $existing_group_id;
-    }
-
-    $account_id = get_post_meta( $post_id, '_ethos_crm_account_id', true );
-
-    $user_id = get_contact( $contact_id, $account_id ) ?? 0;
-
-    $level_id = get_pmpro_level_id( $post_id, $level_name );
-
-    $group = \hacklabr\create_pmpro_group( $user_id, $level_id );
-
-    wp_update_user([
-        'ID' => $user_id,
-        'meta_input' => [
-            '_ethos_admin' => '1',
-            '_pmpro_group' => $group->id,
-            '_pmpro_role' => 'primary',
-        ],
-    ]);
-
-    wp_update_post([
-        'ID' => $post_id,
-        'post_author' => $user_id,
-        'meta_input' => [
-            '_pmpro_group' => $group->id,
-        ],
-    ]);
-
-    approve_contact( $user_id, $level_id );
-
-    return $group->id;
-}
-
-function set_secondary_contact( int $post_id, string $contact_id ) {
-    $account_id = get_post_meta( $post_id, '_ethos_crm_account_id', true );
-
-    $user_id = get_contact( $contact_id, $account_id ) ?? 0;
-
-    if ( empty( get_user_meta( $user_id, '_pmpro_group', true ) ) ) {
-        add_contact_to_organization( $user_id, $post_id );
-    }
-
-    update_user_meta( $user_id, '_ethos_admin', '1' );
-}
-
-function set_approver( int $post_id, string $contact_id ) {
-    $account_id = get_post_meta( $post_id, '_ethos_crm_account_id', true );
-
-    $user_id = get_contact( $contact_id, $account_id ) ?? 0;
-
-    if ( empty( get_user_meta( $user_id, '_pmpro_group', true ) ) ) {
-        add_contact_to_organization( $user_id, $post_id );
-    }
-
-    update_user_meta( $user_id, '_ethos_approver', '1' );
 }
 
 function import_accounts_command( array $args, array $assoc_args ) {
@@ -393,7 +216,7 @@ function import_accounts_command( array $args, array $assoc_args ) {
         foreach( $accounts as $account ) {
             try {
                 \hacklabr\cache_crm_entity( $account );
-                import_account( $account, $force_update );
+                \ethos\crm\import_account( $account, $force_update );
                 $count++;
             } catch ( \Throwable $err ) {
                 cli_log( $err->getMessage(), 'error' );
@@ -468,3 +291,26 @@ function register_import_accounts_command() {
     }
 }
 add_action( 'init', 'ethos\\migration\\register_import_accounts_command' );
+
+function log_message( string $message, string $level = 'debug' ) {
+    if ( class_exists( '\WP_CLI' ) ) {
+        cli_log( $message, $level );
+    } else {
+        error_log( '[' . $level . '] ' . $message, 0 );
+    }
+
+    switch ( $level ) {
+        case 'warning':
+            $logger_status = 'warning';
+            break;
+        case 'error':
+            $logger_status = 'error';
+            break;
+        default:
+            $logger_status = 'info';
+            break;
+    }
+
+    do_action( 'logger', $message, $logger_status );
+}
+add_action( 'ethos_crm:log', 'ethos\\migration\\log_message', 10, 2 );
