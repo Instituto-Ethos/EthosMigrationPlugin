6518b168e02ab8553a31393fb9fdf328ecdda615
leopiccionia <leopiccionia@gmail.com>
2025-09-17 15:48:34 -0300

    Bump plugin version

--- EthosMigrationPlugin.php
+++ EthosMigrationPlugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Ethos Migration Plugin
  * Description:       Plugin to migrate post types, taxonomies and metadata on Ethos site.
- * Version:           0.5.4
+ * Version:           0.5.5
  * Requires at least: 5.2
  * Requires PHP:      7.4
  * Author:            Hacklab
