f34f6ebdc3523379239e3b5952264982e8e7bbe3
leopiccionia <leopiccionia@gmail.com>
2026-02-11 17:18:39 -0300

    Add 2nd first-access WP-CLI script

--- includes/crm.php
+++ includes/crm.php
@@ -46,6 +46,7 @@ function csv_init( string|null $filename = null ) {
         'Conta - ID',
         'Conta - Nome',
         'Usuário - Link de recuperação de senha',
+        'Usário - É admin',
     ] );
 }
 
@@ -58,6 +59,8 @@ function csv_add_line( int $user_id, Entity $account ) {
 
     $recovery_link = sprintf( get_home_url() . '/wp-login.php?action=rp&key=%s&login=%s&lang=pt_BR', get_password_reset_key( $user ), $user->user_login  );
 
+    $ethos_is_admin = ! empty( get_user_meta( $user_id, '_ethos_admin', true ) );
+
     fputcsv( $ethos_crm_csv, [
         $account_id,
         $user->ID,
@@ -67,6 +70,7 @@ function csv_add_line( int $user_id, Entity $account ) {
         $account->Id,
         $account->Attributes['name'] ?? '',
         $recovery_link,
+        $ethos_is_admin ? 'Sim' : 'Não',
     ] );
 }
 
@@ -212,6 +216,101 @@ function first_access_command( array $args ) {
     csv_finish();
 }
 
+function first_access_v2_command() {
+    global $ethos_crm_command;
+    $ethos_crm_command = 'first-access';
+
+    set_hacklab_as_current_user();
+    csv_init();
+
+    $skipped_cnpjs = [
+        // ALLIA HIGIENE
+        '25983227000125',
+        // ALSTOM BRASIL ENERGIA E TRANSPORTE LTDA
+        '88309620000158',
+        '88309620000662',
+        // ASSAÍ ATACADISTA
+        '06057223000171',
+        // BANCO DO BRASIL S.A.
+        '00000000000191',
+        // DANIEL ADVOGADOS
+        '33073800000434',
+        // FEDERAÇÃO DAS INDÚSTRIAS DO ESTADO DO RIO DE JANEIRO - FIRJAN
+        '42422212000107',
+        // RIO ÔNIBUS - SINDICATO DAS EMPRESAS DE ÔNIBUS DA CIDADE DO RIO DE JANEIRO
+        '33927872000159',
+        // TECHINT ENGENHARIA E CONSTRUÇÃO S/A
+        '61575775000180',
+        // INTEX BANK BANCO DE CAMBIO S.A.
+        '02992317000187',
+        // ISA ENERGIA BRASIL S.A
+        '02998611000104',
+        // OPERADOR NACIONAL DO SISTEMA ELÉTRICO
+        '02831210000238',
+        // BOCA ROSA COMPANY LTDA
+        '22694602000129',
+        // LIGA INDEPENDENTE DO GRUPO A - RIO DE JANEIRO
+        '28326598000122',
+        // EQUATORIAL ENERGIA S/A
+        '03220438000173',
+    ];
+
+    $accounts = \hacklabr\iterate_crm_entities( 'account', [
+        'orderby' => 'name',
+        'order' => 'ASC',
+    ] );
+
+    $count = 0;
+
+    foreach ( $accounts as $account ) {
+        $attributes = $account->Attributes;
+        $account_name = $attributes['name'] ?? '';
+        $cnpj = $attributes['fut_st_cnpjsemmascara'] ?? '';
+
+        if ( empty( $cnpj ) || in_array( $cnpj, $skipped_cnpjs ) ) {
+            cli_log( "Skipping {$account_name} ({$account->Id})...");
+            continue;
+        }
+
+        try {
+            cli_log( "Importing {$account_name} ({$account->Id})...");
+            \hacklabr\cache_crm_entity( $account );
+            crm\import_account( $account, true );
+        } catch ( \Throwable $err ) {
+            cli_log( $err->getMessage(), 'error' );
+        }
+
+        $contacts = \hacklabr\iterate_crm_entities( 'contact', [
+            'filters' => [
+                'accountid' => $account->Id,
+            ],
+        ] );
+
+        foreach ( $contacts as $contact ) {
+            try {
+                \hacklabr\cache_crm_entity( $contact );
+                crm\import_contact( $contact, $account, true );
+
+                $user_id = crm\get_contact( $contact->Id, $account->Id );
+                if ( $user_id ) {
+                    csv_add_line( $user_id, $account );
+                    $count++;
+
+                    if ( ( $count % 10 ) == 0 ) {
+                        cli_log( "Imported {$count} contacts..." );
+                    }
+                }
+            } catch ( \Throwable $err ) {
+                cli_log( $err->getMessage(), 'error' );
+            }
+        }
+    }
+
+    cli_log( "Finished importing {$count} contacts.", 'success' );
+
+    csv_finish();
+}
+
 function disable_pmpro_emails( $pre, $option ) {
     if ( inside_wp_cli() ) {
         if ( str_starts_with( $option, 'pmpro_email_' ) && str_ends_with( $option, '_disabled' ) ) {
@@ -259,6 +358,7 @@ add_filter( 'password_reset_expiration', 'ethos\\migration\\change_password_expi
 function register_first_access_command() {
     if ( inside_wp_cli() ) {
         \WP_CLI::add_command( 'first-access', 'ethos\\migration\\first_access_command' );
+        \WP_CLI::add_command( 'first-access-v2', 'ethos\\migration\\first_access_v2_command' );
     }
 }
 add_action( 'init', 'ethos\\migration\\register_first_access_command' );
